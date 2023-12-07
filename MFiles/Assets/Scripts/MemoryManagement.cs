9f4c804fd954f98fc4559242a8ea74bf2207bea0
Yangying <ryysophie@126.com>
2023-12-07 00:10:01 -0800

    UpdateMemorySystem

--- MFiles/Assets/Scripts/MemoryManagement.cs
+++ MFiles/Assets/Scripts/MemoryManagement.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Fungus;
 
 public class MemoryManagement : MonoBehaviour
 {
-    public string[] MemoryCode;
+    public string[] MemoryCodes;
     public Sprite[] MemoryOverview;
     public Sprite NoSignal;
+    public string MemoryCode;
+    public Flowchart flowchart;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +20,6 @@ public class MemoryManagement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      flowchart.SetStringVariable("MemoryCode",MemoryCode);  
     }
 }
--- MFiles/Assets/Scripts/MemoryTimeSelect.cs
+++ MFiles/Assets/Scripts/MemoryTimeSelect.cs
@@ -28,16 +28,24 @@ public class MemoryTimeSelect : MonoBehaviour
     }
 
     public void CheckMemory(){
-    string[] MemoryCodes=MemoryManagement.GetComponent<MemoryManagement>().MemoryCode;
+    string[] MemoryCodes=MemoryManagement.GetComponent<MemoryManagement>().MemoryCodes;
     Sprite[] MemoryOverviews=MemoryManagement.GetComponent<MemoryManagement>().MemoryOverview;
     string MemoryCode=Month.ToString()+"."+Day.ToString();
     for(int i=0; i<MemoryCodes.Length;i++){
         if(MemoryCode==MemoryCodes[i]){
         MemoryOverview.GetComponent<Image>().sprite=MemoryOverviews[i];
         Debug.Log("work");
+        MemoryManagement.GetComponent<MemoryManagement>().MemoryCode=MemoryCode;
+        for(int j=0;j<MemoryOverview.transform.childCount;j++){
+        MemoryOverview.transform.GetChild(j).GetComponent<Image>().sprite=MemoryOverview.transform.GetChild(j).GetComponent<MemoryOverview>().OverviewPicture[i];
+        }
         }
         else{
         MemoryOverview.GetComponent<Image>().sprite=MemoryManagement.GetComponent<MemoryManagement>().NoSignal;
+        for(int j=0;j<MemoryOverview.transform.childCount;j++){
+        MemoryOverview.transform.GetChild(j).GetComponent<Image>().sprite=MemoryOverview.transform.GetChild(j).GetComponent<MemoryOverview>().NoSignal;
+        }
+
         }
     }
 
